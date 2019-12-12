be3125edb8b0516a4571bdc05d9d7015a6f2a386
Dilbazsuleyman <56804853+Dilbazsuleyman@users.noreply.github.com>
2019-12-11 21:08:05 -0500

    Initial commit

---
+++ src/Game/Menu.java
@@ -0,0 +1,8 @@
+package Game;
+
+public class Menu extends GameObject {
+    @Override
+    public void draw() {
+        System.out.println("Menu draw");
+    }
+}
---
+++ src/Game/Player.java
@@ -0,0 +1,9 @@
+package Game;
+
+public class Player extends GameObject {
+    @Override
+    public void draw() {
+        System.out.println("Player draw");
+
+    }
+}
